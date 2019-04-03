da86f4f5013c756d5b80a3f068b6970bffb8c857
Max Tzschoppe <max.tzschoppe@st.ovgu.de>
2019-04-03 11:19:24 +0100

    facetracking code test

--- Algorithm_OpenCV/src/main.cpp
+++ Algorithm_OpenCV/src/main.cpp
@@ -87,11 +87,23 @@ int main(int arg_num, char *arg_vec[]) {
     int ret = 0;
     libraw_processed_image_t *image = iProcessor.dcraw_make_mem_image(&ret);
 
-    auto img = cv::Mat(image->height, image->width, CV_16UC3);
+    auto img = cv::Mat(image->height, image->width, CV_16UC3); //CV_16UC3
 
-    //std::string cascadeFrontalfilename = "/home/mtzschoppe/Desktop/algorithm_opencv/portrait.nef";
-    cv::Ptr<cv::CascadeClassifier> cascade = makePtr<cv::CascadeClassifier>(img);
+    for (int i = 0; i < image->height; i++) {
+        for (int j = 0; j < image->width; j++) {
+            int linindex = (i * image->width + j)*3;
+            cv::Vec3s tripel = cv::Vec3s((short)image->data[linindex+2]*256, (short)image->data[linindex +1]*256, (short)image->data[linindex ]*256);
+            img.at<cv::Vec3s>(i, j) = tripel;
+        }
+    }
+
+    std::string cascadeFrontalfilename = samples::findFile("/home/mtzschoppe/Documents/git/3D-VSoC-demonstrator/Algorithm_OpenCV/opencv/opencv/data/lbpcascades/lbpcascade_frontalface.xml");
+
+    cv::Ptr<cv::CascadeClassifier> cascade = makePtr<cv::CascadeClassifier>(cascadeFrontalfilename);
     cv::Ptr<DetectionBasedTracker::IDetector> MainDetector = makePtr<CascadeDetectorAdapter>(cascade);
+
+    //cv::Ptr<cv::CascadeClassifier> cascade = makePtr<cv::CascadeClassifier>(img);
+    //cv::Ptr<DetectionBasedTracker::IDetector> MainDetector = makePtr<CascadeDetectorAdapter>(cascade);
     /*
     if ( cascade->empty() )
     {
@@ -100,8 +112,11 @@ int main(int arg_num, char *arg_vec[]) {
     }
     */
 
-    cascade = makePtr<cv::CascadeClassifier>(img);
+    cascade = makePtr<cv::CascadeClassifier>(cascadeFrontalfilename);
     cv::Ptr<DetectionBasedTracker::IDetector> TrackingDetector = makePtr<CascadeDetectorAdapter>(cascade);
+
+    //cascade = makePtr<cv::CascadeClassifier>(img);
+    //cv::Ptr<DetectionBasedTracker::IDetector> TrackingDetector = makePtr<CascadeDetectorAdapter>(cascade);
     /*
     if ( cascade->empty() )
     {
@@ -140,6 +155,6 @@ int main(int arg_num, char *arg_vec[]) {
 
     Detector.stop();
     iProcessor.recycle();
-    
+
     return 0;
 }
