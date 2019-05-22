071337e68a11bbfb5526ce34ee90fe4bfeb24ede
Max Tzschoppe <max.tzschoppe@st.ovgu.de>
2019-05-22 17:28:39 +0100

    changes to factory

---
+++ Algorithm_OpenCV/src/config.cpp
@@ -0,0 +1,6 @@
+//
+// Created by mtzschoppe on 22/05/19.
+//
+
+// Sensor -> n ADC's -> crop image in n sections
+
--- Algorithm_OpenCV/src/factory.h
+++ Algorithm_OpenCV/src/factory.h
@@ -5,4 +5,20 @@
 #ifndef RAWTOCV_FACTORY_H
 #define RAWTOCV_FACTORY_H
 
+//singleton
+/*
+#pragma once
+
+#include <vector>
+#include <model/traffic/Packet.h>
+
+class PacketFactory {
+public:
+    std::vector<Packet*> packets;
+    static PacketFactory& getInstance();
+    Packet* createPacket(Node& src, Node& dst, int size, double generationTime, dataTypeID_t dataType);
+    void deletePacket(Packet* p);
+};
+ */
+
 #endif //RAWTOCV_FACTORY_H
