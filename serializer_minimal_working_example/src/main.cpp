d060600182eeb510ce173599810c6a34e487f36c
Jan Moritz Joseph <jan.joseph@ovgu.de>
2019-07-01 17:27:11 +0200

    serializer working

--- serializer_minimal_working_example/src/main.cpp
+++ serializer_minimal_working_example/src/main.cpp
@@ -7,8 +7,34 @@
 #include <iostream>
 
 int main (int argc, char *argv[]) {
-    Packet p1;
     Packet p2;
+    Packet p3;
+    Packet p4;
+    Packet p1;
+    p1.addr_src = 100;
+    p1.dst = 99;
+    p1.data.emplace_back(0b11);
+    std::cout << p1 << std::endl;
+    for (auto i : p1.data){
+        std::cout << i << std::endl;
+    }
 
-    std::cout << p2 << std::endl;
+    std::ofstream ofs("filename");
+    {
+        boost::archive::text_oarchive oa(ofs);
+        oa << p1;
+    }
+
+    Packet newP;
+    {
+        std::ifstream ifs("filename");
+        boost::archive::text_iarchive ia(ifs);
+        ia >> newP;
+    }
+
+    std::cout << newP << std::endl;
+
+    for (auto i : newP.data){
+        std::cout << i << std::endl;
+    }
 }
--- serializer_minimal_working_example/src/structures.h
+++ serializer_minimal_working_example/src/structures.h
@@ -12,6 +12,11 @@
 #include <set>
 #include <vector>
 #include <iostream>
+#include <fstream>
+
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/serialization/vector.hpp>
 
 typedef int addr_t;
 typedef int node_t;
@@ -21,15 +26,34 @@ typedef long long id_type;
 class Packet {
 private:
     static id_type idcnt;
+    friend class boost::serialization::access;
+
+    template<class Archive>
+    void serialize(Archive & ar, const unsigned int version)
+    {
+        ar & id;
+        ar & cyck;
+        ar & datatyp;
+        ar & addr_src;
+        ar & dst;
+        ar & node_type;
+        ar & num_deps;
+        ar & deps;
+        ar & data;
+    }
+
 public:
     id_type id = 0;
     long long cyck = 0;
-    data_t Datatyp = 0;
+    data_t datatyp = 0;
     addr_t addr_src = 0;
     addr_t dst = 0;
     node_t node_type = 0;
     int num_deps = 0;
-    //std::vector<id_t> deps;
+    std::vector<id_t> deps;
+    std::vector<char> data;
+
+
 
     Packet();
 
@@ -37,5 +61,9 @@ public:
     friend bool operator==(const Packet& p1, const Packet& p2);
     friend bool operator!=(const Packet& p1, const Packet& p2);
 };
+
+// How to serialize:
+//https://isocpp.org/wiki/faq/serialization#serialize-no-inherit-no-ptrs
+//How do I serialize objects that contain pointers to other objects, but those pointers form a tree with no cycles and no joins?
 
 #endif //RAWTOCV_STRUCTURES_H
